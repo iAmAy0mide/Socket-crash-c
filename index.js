0f0912e7d03f51565241a13e734f83bd0c53ba85
Ayomide <148401865+iAmAy0mide@users.noreply.github.com>
2024-08-29 13:59:59 +0000

    ups

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
- 
+import express from "express";
+import http from "http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const f = fileURLToPath(import.meta.url);
+console.log(f)
+console.log(__dirname)
+
+app.get("", (req, res) => res.sendFile(join(__dirname, "index.html")));
+
+const PORT = 3000;
+
